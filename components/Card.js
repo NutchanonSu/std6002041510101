9f19c1c72f7d531446bc059ad322223d6f14b078
ณัฐชนน สุรจินตนาภรณ์ <s6002041510101@Macmini-213-04.local>
2019-03-28 16:07:27 +0700

    adding style of button

--- components/Card.js
+++ components/Card.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, Button } from 'react-native';
 import CardSection from './CardSection';
 
 class Card extends Component {
     render() {
         return (
             <View>
-                <CardSection />
-                <CardSection />
-                <CardSection />
+                <Text>Meteora</Text>
+                <Text>Linkin Park</Text>
+                <CardSection>
+                <Image
+                    style={{width: 420, height: 400}}
+                    source={{uri: 'https://goo.gl/1EBWpY'}}
+                />
+                </CardSection>
+                <CardSection>
+                    <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 10 }}>
+                    <View style={{ width: 100, backgroundColor: 'red' }}>
+                    <Button 
+                        title="Buy"
+                    />
+                    </View>
+                    </View>
+                </CardSection>
             </View>
         );
+    }
+}
+
+const style = {
+    button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10
     }
 }
 
