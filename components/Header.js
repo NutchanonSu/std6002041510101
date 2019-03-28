e288c893738dfeae4171886d4f3a1087c90297cf
ณัฐชนน สุรจินตนาภรณ์ <s6002041510101@Macmini-213-04.local>
2019-03-28 14:23:10 +0700

    using Header component on App

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+// inport library
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+// create component
+class Header extends Component {
+    render() {
+        return (
+            <View>
+                <Text>{this.props.title}</Text>
+            </View>
+        );
+    }
+}
+
+// export 
+export default Header;
