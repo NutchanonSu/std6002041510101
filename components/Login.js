4c08d92239de97af96bc306d23b4e59586509cb5
ณัฐชนน สุรจินตนาภรณ์ <s6002041510101@Macmini-213-04.local>
2019-04-04 16:56:46 +0700

    create Login

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,7 @@
 // import library
 import React, { Component } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
+import axios from 'axios';
 
 // write component
 class Login extends Component {
@@ -10,13 +11,22 @@ class Login extends Component {
             email: '',
             password: ''
         }
-        this.onChangeEmail = this.onChangeEmail.bind()
+        this.onChangeEmail = this.onChangeEmail.bind(this)
     }
     onChangeEmail(e) {
         console.log('onChangeEmail', e)
-        this.setState({
-            email: e.target
-        })
+        this.setState({ email: e})
+    }
+    onChangePassword(e){
+        this.setState({ password: e})
+    }
+    onPress() {
+        console.log(this.state)
+        const url = 'http://128.199.240.120:9999/api/auth/login'
+        axios.post(url, this.state)
+            .then(responce => {
+                console.log('token', Response.data.data.token)
+            })
     }
     render () {
         return (
@@ -25,16 +35,18 @@ class Login extends Component {
                     style={{ height: 40, fontWeight: 'bold', fontSize: 18 }}
                     placeholder="Email"
                     value={this.state.email}
-                    onChangeText={this.onChangeEmail} 
+                    onChangeText={this.onChangeEmail.bind(this)} 
                 />
                 <TextInput 
                     secureTextEntry
                     style={{ height: 40, fontWeight: 'bold', fontSize: 18 }}
                     placeholder="Password"
                     value={this.state.password}
+                    onChangeText={this.onChangePassword.bind(this)}
                 />
                 <Button 
                     title="Login"
+                    onPress={this.onPress.bind(this)}
                 />
             </View>
         );
